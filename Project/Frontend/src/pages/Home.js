837983acba5a66ab0f4b4d8851f405b6917330d6
harshpatel2212 <harshup2212@gmail.com>
2023-03-11 18:36:23 +0530

    Feature: Added User Profile Module

---
+++ Project/Frontend/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const Home = () => {
+  return <Typography variant="h1"> Hey Welcome!</Typography>;
+};
+
+export default Home;
