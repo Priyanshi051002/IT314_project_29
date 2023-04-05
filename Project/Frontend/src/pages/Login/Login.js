f5029f2a00965adf589ba42d1ade5f8ca2d4f88d
Kaushal Patel <kaushal290902@gmail.com>
2023-04-06 00:34:13 +0530

    Fetching Backend Data for Frontends Login and Signup function

--- Project/Frontend/src/pages/Login/Login.js
+++ Project/Frontend/src/pages/Login/Login.js
@@ -1,53 +1,127 @@
-import React from 'react'
-import {Avatar, Button, FormControlLabel, Grid, Paper, TextField, Typography} from '@mui/material'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Checkbox from '@mui/material/Checkbox';
-import { Link } from 'react-router-dom';
+import React,{useState} from "react";
+import {
+  Avatar,
+  Button,
+  FormControlLabel,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Checkbox from "@mui/material/Checkbox";
+import { Link, redirect } from "react-router-dom";
 
+const Login = ({ handleChange }) => {
 
-const Login = ({handleChange}) => {
-  const paperStyle = {padding: 20, height: '70vh', width: 300, margin: "0 auto"}
-  const avatarStyle = {backgroundColor: '#1bbd7e'}
-  const buttonStyle={margin:'8px 0'}
+  const [details, setDetails] = useState({
+    username: "",
+    password: "",
+  });
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setDetails((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSignin = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:5000/login`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify(details),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if(data.success){
+          return redirect("/");
+        }
+        else{
+          return null;
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 300,
+    margin: "0 auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const buttonStyle = { margin: "8px 0" };
   return (
     <div>
       <Grid>
         <Paper elevation={20} style={paperStyle}>
-          <Grid align='center'>
-          <Avatar style={avatarStyle}>
-            <LockOutlinedIcon/>
-          </Avatar>
-          <h2>Sign in</h2>
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <h2>Sign in</h2>
           </Grid>
-          <TextField variant="filled" label='Username' required placeholder='Enter username' fullWidth='true' margin='10 px'></TextField>
-          <TextField variant="filled" label='Password' required placeholder='Enter password'  type='password' fullWidth='true'></TextField>
-          <FormControlLabel
-          control={
-            <Checkbox
-              name="checkedB"
-              color="primary"
+          <form onSubmit={handleSignin}>
+            <TextField
+              variant="filled"
+              label="Username"
+              name="username"
+              value={details.username}
+              required
+              onChange={changeHandler}
+              placeholder="Enter username"
+              fullWidth="true"
+              margin="10 px"
+            ></TextField>
+            <TextField
+              variant="filled"
+              label="Password"
+              name="password"
+              value={details.password}
+              required
+              placeholder="Enter password"
+              type="password"
+              onChange={changeHandler}
+              fullWidth="true"
+            ></TextField>
+            <FormControlLabel
+              control={<Checkbox name="checkedB" color="primary" />}
+              label="Remember me"
             />
-          }
-          label="Remember me"
-    />
-    <Button type='submit' variant="contained" color="primary" fullWidth='true' style={buttonStyle}>Sign in</Button>
-    <Typography>
-      
-     <Link to="/pc">Forgot Password?</Link>
-
-      
-    </Typography>
-    <Typography> Do you have an account?
-     <Link href="#" onClick={
-      ()=>handleChange("event",1)
-     } >
-     Sign Up
-</Link> 
-    </Typography>
-           </Paper>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth="true"
+              style={buttonStyle}
+            >
+              Sign in
+            </Button>
+          </form>
+          <Typography>
+            <Link to="/pc">Forgot Password?</Link>
+          </Typography>
+          <Typography>
+            {" "}
+            Do you have an account?
+            <Link href="#" onClick={() => handleChange("event", 1)}>
+              Sign Up
+            </Link>
+          </Typography>
+        </Paper>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
