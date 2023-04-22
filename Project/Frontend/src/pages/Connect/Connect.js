b81e985e343fc5f259de95625a59e90ec9867718
harshpatel2212 <harshup2212@gmail.com>
2023-04-22 19:57:22 +0530

    Feature: Added Connect with others

--- Project/Frontend/src/pages/Connect/Connect.js
+++ Project/Frontend/src/pages/Connect/Connect.js
@@ -1,156 +1,212 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import {
-//   Card,
-//   CardContent,
-//   Typography,
-//   CardActions,
-//   Button,
-//   CardMedia,
-//   Grid,
+  CardMedia,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Grid,
   Container,
-//   Box,
+  Box,
+  Divider,
 } from "@mui/material";
+
 // import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 
-// const DUMMY_DATA = [
-//   {
-//     name: "Om",
-//     source: "https://source.unsplash.com/random",
-//     description:
-//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
-//   },
-//   // {
-//   //   name: "Harsh",
-//   //   source: "https://source.unsplash.com/random",
-//   //   description:
-//   //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
-//   // },
-//   // {
-//   //   name: "Priyanshi",
-//   //   source: "https://source.unsplash.com/random",
-//   //   description:
-//   //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
-//   // },
-//   // {
-//   //   name: "Kaushal",
-//   //   source: "https://source.unsplash.com/random",
-//   //   description:
-//   //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
-//   // },
-//   // {
-//   //   name: "Devdeep",
-//   //   source: "https://source.unsplash.com/random",
-//   //   description:
-//   //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
-//   // },
-//   // {
-//   //   name: "Achyut",
-//   //   source: "https://source.unsplash.com/random",
-//   //   description:
-//   //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
-//   // },
-// ];
-
-// const postCard = (
-//   <React.Fragment>
-//     <Card>
-//       <CardContent>
-//         <Typography variant="h5" component="div" gutterBottom>
-//           My Posts
-//         </Typography>
-//         <Grid container spacing={2}>
-//           <Grid item xs={12} sm={6} md={6}>
-//             <PostCards items={DUMMY_DATA} />
-//           </Grid>
-//           <Grid item xs={12} sm={6} md={6}>
-//             <PostCards items={DUMMY_DATA} />
-//           </Grid>
-//         </Grid>
-//       </CardContent>
-//       <CardActions>
-//         <Button
-//           variant="contained"
-//           component={Link}
-//           to="/myposts"
-//           fullWidth
-//           color="success"
-//         >
-//           Show All Posts
-//         </Button>
-//       </CardActions>
-//     </Card>
-//   </React.Fragment>
-// );
-
-// const profileCard = (
-//   <React.Fragment>
-//     <Card>
-//       <CardMedia
-//         component="img"
-//         height="250"
-//         image={PROFILE_DATA.bg_image}
-//       ></CardMedia>
-
-//       <Grid container>
-//         <Grid item xs={12} sm={5} md={4} py={3}>
-//           <CardMedia
-//             component="img"
-//             image={PROFILE_DATA.prf_image}
-//             sx={{
-//               margin: "auto",
-//               width: "200px",
-//               height: "200px",
-//               borderRadius: "50%",
-//               maxWidth: "100%",
-//             }}
-//           ></CardMedia>
-//         </Grid>
-//         <Grid item xs={12} sm={7} md={8}>
-//           <CardContent>
-//             <Grid container>
-//               <Grid item xs={8} sm={8} md={8}>
-//                 <Typography variant="h4" component="div" gutterBottom>
-//                   {PROFILE_DATA.name}
-//                 </Typography>
-//               </Grid>
-//               <Grid item xs={2} sm={2} md={2}>
-//                 <Typography variant="h6" component="div" gutterBottom>
-//                   120
-//                 </Typography>
-//               </Grid>
-//               <Grid item xs={2} sm={2} md={2}>
-//                 <Typography variant="h6" component="div" gutterBottom>
-//                   120
-//                 </Typography>
-//               </Grid>
-//             </Grid>
-//             <Typography variant="body2" height={80} gutterBottom>
-//               {PROFILE_DATA.description}
-//             </Typography>
-//           </CardContent>
-//           <CardActions>
-//             <Button size="small" variant="contained">
-//               Edit Profile
-//             </Button>
-//             <Button size="small" variant="contained">
-//               Share Profile
-//             </Button>
-//             <Button size="small" variant="contained">
-//               Add Post
-//             </Button>
-//           </CardActions>
-//         </Grid>
-//       </Grid>
-//     </Card>
-//   </React.Fragment>
-// );
+const users = [
+  {
+    name: "Om",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Harsh",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Priyanshi",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Kaushal",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Devdeep",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Achyut",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Katrina",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Kathan",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Krish",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Sanjay",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Ram",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Zeel",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Devdeep",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Achyut",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Katrina",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Kathan",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Krish",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Sanjay",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Ram",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+  {
+    name: "Zeel",
+    source: "https://source.unsplash.com/random",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Mus mauris vitae ultricies leo integer malesuada nunc.",
+  },
+];
 
 const Connect = () => {
+  // const [users, setUsers] = useState([]);
+  const [visible, setVisible] = useState(12);
+
+  //   useEffect(() => {
+  //     fetch("https://jsonplaceholder.typicode.com/posts")
+  //       .then((res) => res.json())
+  //       .then((data) => setUsers(data));
+  //   }, []);
+
+  const ShowMoreItems = () => {
+    setVisible((prevValue) => prevValue + 4);
+  };
+
   return (
     <>
       <Container maxWidth="lg">
-        <Navbar />
+        <Box>
+          <Navbar />
+          <Typography
+            variant="h4"
+            textAlign={"center"}
+            component="div"
+            gutterBottom
+          >
+            Connect With Others
+          </Typography>
+          <Divider />
+          <Grid container rowSpacing={3} columnSpacing={4} mt ={0.5} mb = {2}>
+            {users.slice(0, visible).map((user) => (
+              <Grid item xs={12} sm={6} md={3}>
+                <Card>
+                  <CardMedia
+                    component="img"
+                    alt="green iguana"
+                    height="125"
+                    image={user.source}
+                  />
+                  <CardContent>
+                    <Typography variant="h5" textAlign={"left"} component="div">
+                      {user.name}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      size="small"
+                      sx={{ borderRadius: "16px" }}
+                      fullWidth
+                      color="info"
+                      variant="outlined"
+                    >
+                      Connect
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+          <Box mt={2} mb={2}>
+            <Button
+              size="large"
+              color="success"
+              fullWidth
+              onClick={ShowMoreItems}
+            >
+              Load more
+            </Button>
+          </Box>
+        </Box>
       </Container>
     </>
   );
