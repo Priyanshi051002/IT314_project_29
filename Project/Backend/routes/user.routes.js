7cf1b6f8c0b4a03c26cc5ffb4f97da2122e85132
Achyut-1412 <202001449@daiict.ac.in>
2023-04-22 21:28:22 +0530

    Integrating code

--- Project/Backend/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { loginfunc, registerfunc } = require("../controllers/register.controller");
-const router = express.Router();
-
-router.post("/login", loginfunc);
-router.post("/register", registerfunc);
-module.exports = router;
