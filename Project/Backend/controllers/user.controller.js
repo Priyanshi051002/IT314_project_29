bc83103e64a491b857e8523155aa87c5a66f94ba
harshpatel2212 <harshup2212@gmail.com>
2023-04-26 02:45:52 +0530

    Fixed few bugs

--- Project/Backend/controllers/user.controller.js
+++ Project/Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const User = require("../models/register.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 
 exports.login = async (req, res) => {
   try {
@@ -12,27 +12,29 @@ exports.login = async (req, res) => {
     if (isMatch) {
       user.password = null;
       user.confirmPassword = null;
-      console.log(process.env.ACCESS_TOKEN_SECRET);
-     const acesstoken = jwt.sign({username : user.username},process.env.ACCESS_TOKEN_SECRET,(err,token)=>{
-        if(err){
-          console.log(err);
-          return res.json({
-            success: false,
-            error: "Error!",
-          });
+      const acesstoken = jwt.sign(
+        { username: user.username },
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, token) => {
+          if (err) {
+            console.log(err);
+            return res.json({
+              success: false,
+              error: "Error!",
+            });
+          } else {
+            // req.user = user;
+            // console(req.user.username);
+            return res.json({
+              success: true,
+              data: token,
+              error: "",
+            });
+          }
         }
-        else{
-          // req.user = user;
-          // console(req.user.username);
-          return res.json({
-            success: true,
-            data: token,
-            error: "",
-          });
-        }
-      });
+      );
 
-       //user.token = acesstoken;
+      //user.token = acesstoken;
 
       // return res.json({
       //   success: true,
@@ -59,17 +61,10 @@ exports.register = async (req, res) => {
   const user = await User.findOne({ username: req.body.username });
 
   if (user) return res.status(400).send("User already registered");
-  console.log(req.body);
 
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
   req.body.password = hashedPassword;
-  const newUser = await User.create();
-  newUser.name = req.body.name;
-  newUser.username = req.body.username;
-  newUser.password = req.body.password;
-  newUser.birthplace = req.body.birthplace;
-
-  await newUser.save();
+  const newUser = await User.create(req.body);
 
   res.status(201).send({
     data: newUser,
