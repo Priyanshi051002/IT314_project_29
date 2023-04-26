26a867422e95695b27e4d6a27e5b2125eb30190b
Kaushal Patel <kaushal290902@gmail.com>
2023-04-27 00:25:07 +0530

    Added profile model and routes for MongoDB APIs

--- Project/Backend/controllers/user.controller.js
+++ Project/Backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/register.model");
+const Profile = require("../models/profile.model")
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -23,8 +24,6 @@ exports.login = async (req, res) => {
               error: "Error!",
             });
           } else {
-            // req.user = user;
-            // console(req.user.username);
             return res.json({
               success: true,
               data: token,
@@ -33,14 +32,6 @@ exports.login = async (req, res) => {
           }
         }
       );
-
-      //user.token = acesstoken;
-
-      // return res.json({
-      //   success: true,
-      //   data: user,
-      //   error: "",
-      // });
     } else {
       return res.json({
         success: false,
@@ -66,8 +57,266 @@ exports.register = async (req, res) => {
   req.body.password = hashedPassword;
   const newUser = await User.create(req.body);
 
-  res.status(201).send({
+  res.status(200).send({
     data: newUser,
+    success: true,
+    error: "",
+  });
+};
+
+exports.getProfile = async (req, res) => {
+  const user = await User.findOne({ username: req.user.username });
+  if (user) {
+    return res.status(200).send({
+      data: user,
+      success: true,
+      error: "",
+    });
+  } else {
+    return res.status(403).send({
+      data: {},
+      success: false,
+      error: "User not found",
+    });
+  }
+};
+
+exports.createProfile = async (req, res) => {
+  try {
+    console.log(req.body)
+    const { name, description, about } = req.body;
+
+    const body = {
+      name,
+      description,
+      about,
+      user : req.user.username
+    }
+    console.log(body,"body")
+    const profile = await Profile.create(body);
+    console.log(profile,"profile")
+    // await profile.save();
+    return res.status(200).send({
+      data: profile,
+      success: true,
+      error: "",
+    });
+  } catch (err) {
+    return res.status(400).send({
+      data: {},
+      success: false,
+      error: err,
+    });
+  }
+};
+
+exports.editProfile = async (req, res) => {
+  try {
+    //find profile from schema
+    const profile = await Profile.findOne({ user: req.user.username });
+    //update profile
+    //check if not found
+    if (!profile) {
+      return res.status(404).send({
+        data: {},
+        success: false,
+        error: "Profile not found",
+      });
+    } else {
+      profile.name = req.body.name;
+      profile.description = req.body.description;
+      profile.about = req.body.about;
+      await profile.save();
+      return res.status(200).send({
+        data: profile,
+        success: true,
+        error: "",
+      });
+    }
+  } catch (err) {
+    return res.status(400).send({
+      data: {},
+      success: false,
+      error: err,
+    });
+  }
+};
+
+exports.follow = async (req, res) => {
+  //get the username of the user who is being followed
+  const follwedUser = req.body.username;
+  //get the username of the user who is following
+  const follower = req.user.username;
+
+  //check if the user is already following the user
+
+  //add the follower in the followers array of the user
+  const x = await User.findOneAndUpdate(
+    { username: follwedUser },
+    { $push: { followers: follower } },
+    { new: true }
+  )
+    .then((result) => {
+      res.status(200).send({
+        data: result,
+        success: true,
+        error: "",
+      });
+    })
+    .catch((err) => {
+      res.status(404).send({
+        data: {},
+        success: false,
+        error: err,
+      });
+    });
+
+  //add the user in the following array of the follower
+  const y = await User.findOneAndUpdate(
+    { username: follower },
+    { $push: { following: follwedUser } },
+    { new: true }
+  )
+    .then((result) => {
+      res.status(200).send({
+        data: result,
+        success: true,
+        error: "",
+      });
+    })
+    .catch((err) => {
+      res.status(404).send({
+        data: {},
+        success: false,
+        error: err,
+      });
+    });
+};
+
+exports.unfollow = async (req, res) => {
+  //get the username of the user who is being unfollowed
+  const unfollwedUser = req.body.username;
+  //get the username of the user who is unfollowing
+  const unfollower = req.user.username;
+
+  //delete the unfollower in the followers array of the user
+  const Updatedfolloweduser = await User.findOneAndUpdate(
+    { username: unfollwedUser },
+    { $pull: { followers: unfollower } },
+    { new: true }
+  )
+    .then((result) => {
+      res.status(200).send({
+        data: result,
+        success: true,
+        error: "",
+      });
+    })
+    .catch((err) => {
+      res.status(404).send({
+        data: {},
+        success: false,
+        error: err,
+      });
+    });
+
+  //delete the user in the following array of the unfollower
+  const UpdatedfollowedrserUser = await User.findOneAndUpdate(
+    { username: unfollower },
+    { $pull: { following: unfollwedUser } },
+    { new: true }
+  )
+    .then((result) => {
+      res.status(200).send({
+        data: result,
+        success: true,
+        error: "",
+      });
+    })
+    .catch((err) => {
+      res.status(404).send({
+        data: {},
+        success: false,
+        error: err,
+      });
+    });
+};
+
+exports.forget = async (req, res) => {
+  const { username, birthplace } = req.body;
+  const user = await User.findOne({ username });
+  if (user) {
+    if (user.birthplace === birthplace) {
+      return res.status(200).send({
+        data: {},
+        success: true,
+        error: "",
+      });
+    } else {
+      return res.status(200).send({
+        data: {},
+        success: false,
+        error: "wrong birthplace",
+      });
+    }
+  } else {
+    return res.status(200).send({
+      data: {},
+      success: false,
+      error: "User Not Found",
+    });
+  }
+};
+
+exports.getUserByName = async (req, res) => {
+  //get the users
+  const x = req.body.name;
+  User.find({ name: { $regex: x, $options: "i" } })
+    .then((users) => {
+      return res.status(200).send({
+        data: users,
+        success: true,
+        error: "",
+      });
+    })
+    .catch((err) => {
+      return res.status(404).send({
+        data: {},
+        success: false,
+        error: err,
+      });
+    });
+};
+
+exports.getAllUser = async (req, res) => {
+  //get All the users
+
+  User.find({})
+    .then((users) => {
+      return res.status(200).send({
+        data: users,
+        success: true,
+        error: "",
+      });
+    })
+    .catch((err) => {
+      return res.status(404).send({
+        data: {},
+        success: false,
+        error: err,
+      });
+    });
+};
+
+exports.updatePassword = async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  req.body.password = hashedPassword;
+  const user = await User.findOneAndUpdate(
+    { username: req.body.username },
+    { password: req.body.password }
+  );
+  return res.status(200).send({
+    data: {},
     success: true,
     error: "",
   });
--- Project/Backend/routes/user.routes.js
+++ Project/Backend/routes/user.routes.js
@@ -1,8 +1,29 @@
 const express = require("express");
-const { login, register } = require("../controllers/user.controller");
+const {
+  login,
+  register,
+  getProfile,
+  createProfile,
+  editProfile,
+  follow,
+  unfollow,
+  forget,
+  updatePassword,
+  getAllUser,
+  getUserByName,
+} = require("../controllers/user.controller");
 const router = express.Router();
+const authenticateToken = require("../middlewares/passport");
 
 router.post("/login", login);
 router.post("/register", register);
-
+router.get("/getProfile", authenticateToken, getProfile);
+router.post("/createProfile", authenticateToken, createProfile);
+router.put("/editProfile", authenticateToken, editProfile);
+router.post("/follow", authenticateToken, follow);
+router.post("/unfollow", authenticateToken, unfollow);
+router.post("/forget", forget);
+router.post("/updatepassword", updatePassword);
+router.get("/getAllUser", authenticateToken, getAllUser);
+router.post("/getUserByName", authenticateToken, getUserByName);
 module.exports = router;
