192ae3772f34ee34b8b098d47d98bfb3b4738ece
omp217 <111455228+omp217@users.noreply.github.com>
2023-04-27 22:10:33 +0530

    added deleteindex.js

---
+++ Project/Backend/deleteindex.js
@@ -0,0 +1,11 @@
+const client = require('./connection.js');
+
+client.indices.delete({
+    index: 'post'
+}, (err, result, status) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Post index deleted');
+    }
+});
