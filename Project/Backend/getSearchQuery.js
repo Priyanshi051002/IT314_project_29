34ae820ae2328a44585da4b0e8c1c83aaba8f398
omp217 <111455228+omp217@users.noreply.github.com>
2023-04-27 22:11:03 +0530

    added getSearchQuery.js

---
+++ Project/Backend/getSearchQuery.js
@@ -0,0 +1,72 @@
+// Create a function which will take a string as parameter and return a string
+
+const getSearchQuery = (searchString) => {
+    const query = 
+    {
+      "query": {
+        "bool": {
+          "should": [
+            {
+              "match": {
+                "title": {
+                  "query": searchString,
+                  "boost": 3
+                }
+              }
+            },
+            {
+              "match": {
+                "description": {
+                  "query": searchString,
+                  "boost": 2
+                }
+              }
+            },
+            {
+              "nested": {
+                "path": "comments",
+                "query": {
+                  "match": {
+                    "comments.comment": searchString
+                  }
+                },
+                "inner_hits": {
+                  "highlight": {
+                    "fields": {
+                      "comments.comment": {
+                        "fragment_offset": 0,
+                        "number_of_fragments": 0,
+                        "pre_tags": [
+                          "<mark>"
+                        ],
+                        "post_tags": [
+                          "</mark>"
+                        ]
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          ]
+        }
+      },
+      "highlight": {
+        "fields": {
+          "description": {
+            "fragment_offset": 0,
+            "number_of_fragments": 0,
+            "pre_tags": [
+              "<mark>"
+            ],
+            "post_tags": [
+              "</mark>"
+            ]
+          },
+        }
+      }
+    }
+    return query;
+}
+
+module.exports = getSearchQuery;
