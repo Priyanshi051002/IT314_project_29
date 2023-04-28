36916ce4c0ef2b9476632c89fa8d2101990b83cd
Achyut-1412 <79002869+Achyut-1412@users.noreply.github.com>
2023-04-28 14:01:02 +0530

    Update user.routes.js

--- Project/Backend/routes/user.routes.js
+++ Project/Backend/routes/user.routes.js
@@ -10,18 +10,20 @@ const {
   updatePassword,
   getAllUser,
   getUserByName,
+  getProfileOfUser,
 } = require("../controllers/user.controller");
 const router = express.Router();
 const authenticateToken = require("../middlewares/passport");
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/getProfileOfUser",getProfileOfUser);
 router.get("/getProfile", authenticateToken, getProfile);
 router.post("/createProfile", createProfile);
 router.put("/editProfile", authenticateToken, editProfile);
 router.post("/follow", authenticateToken, follow);
 router.post("/forget", forget);
-router.post("/updatepassword", authenticateToken, updatePassword);
+router.post("/updatepassword", updatePassword);
 router.get("/getAllUser", authenticateToken, getAllUser);
 router.post("/getUserByName", authenticateToken, getUserByName);
 module.exports = router;
